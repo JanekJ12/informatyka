0d0da453363baa3cce15c98e50d6824854e920f0
Janek Jurczuk <152389349+JanekJ12@users.noreply.github.com>
2025-02-18 13:41:37 +0100

    do domu

--- zadania/zadanie66.py
+++ zadania/zadanie66.py
@@ -4,6 +4,18 @@ def suma_cyfr(liczba):
     for cyfra in liczba:
         suma += int(cyfra)
     return suma
+
+def pierwsza(liczba):
+    if liczba < 2:
+        return False
+    if liczba % 2 == 0:
+        return False
+    for i in range(3, int(liczba**0.5)+1, 2):
+        if liczba % i == 0:
+            return False
+
+
+
 lista_trojek = []
 with open("C:/Users/jan.jurczuk/Documents/GitHub/informatyka/zadania/trojki.txt") as pliki:
     for linia in pliki:
@@ -13,3 +25,8 @@ print(lista_trojek)
 for liczby in lista_trojek:
     if suma_cyfr(liczby[0]) + suma_cyfr(liczby[1]) == int(liczby[2]):
         print(liczby)
+
+for liczby in lista_trojek:
+    if int(liczby[0]) + int(liczby[1]) == int(liczby[2]):
+        if pierwsza(int(liczby[0])) and pierwsza(int(liczby[1])):
+            print(liczby)
